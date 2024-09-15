c9b39c61a2d661c0838cceb2123d35b97d5f08f3
avayyyyyyy <jabhi465@gmail.com>
2024-09-15 18:47:20 +0530

    style: improve page layout for better responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import WorkExperience from "@/components/WorkExperience";
 export default function Home() {
   return (
     <div className="relative lg:w-7/12 w-full mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 md:p-20 overflow-x-hidden">
-      <main className="flex flex-col gap-8 row-start-2 items-center md:items-start w-full overflow-hidden">
+      <main className="flex flex-col gap-8 px-2 row-start-2 items-center md:items-start w-full overflow-hidden">
         <Intro />
         <WorkExperience />
         <Skills />
